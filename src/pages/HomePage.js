a3d832f3b75982ff64608fc9b1b5902ece0dec7c
VikasWilliam <vikasdenobili@outlook.com>
2025-07-09 21:48:11 +0530

    fourth commit: used grid layout to display products on home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,53 @@
 import { LitElement, html, css } from "lit-element";
+import storeItems from "../data/items.json";
+import "../header/MyButton";
 
 export class HomePage extends LitElement {
+    static styles = css`
+    .product{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        padding: 20px;
+        gap: 80px;
+        width: 100%;
+        margin-left:50px;
+    }
+    .product_item {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 4px;
+    background-color: #f9f9f9;
+    text-align: center;
+    height: 225px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+    transition: transform 0.1s;
+  }
+
+  .product_item:hover {
+    transform: scale(1.05);
+  }
+
+  .product_item img {
+    max-height: 180px;
+    width: auto;
+    object-fit: contain;
+  }
+  .product_name{
+    display:flex;
+    margin-top :15px;
+    gap:25px
+  
+}     
+  }
+   `
+
+    handleClick(e) {
+        alert("button clicked")
+    }
     constructor() {
         super();
     }
@@ -11,7 +58,19 @@ export class HomePage extends LitElement {
 
     render() {
         return html`
-         <div>This is home page</div>
+         <div class="product">
+             
+             ${storeItems.map(product => html`
+                <div class="product_item">
+                    <img src=${product.imgUrl} alt=${product.name}/>
+                    <div class="product_name">
+                        <div>${product.name}</div>
+                        <my-button label="apply" ?primary=true size="small" backgroundColor="orange" @click=${this.handleClick}></my-button>
+                    </div>  
+                </div>
+             `)}
+             
+         </div>
         `
     }
 }
